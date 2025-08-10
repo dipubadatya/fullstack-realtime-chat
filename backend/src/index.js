56afd2388f08b806264411b26d168bd76218d0b8
dipubadatya <148934509+dipubadatya@users.noreply.github.com>
2025-08-10 19:02:35 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,12 +29,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  // Correct path: Look for 'frontend/dist' inside the current directory
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
 
+  // Correct path for the fallback route
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
   });
 }
+
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
